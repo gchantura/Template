46879537f565346e3cb75afaaab8cbdb1ce0c9e6
CheGe <89837796+gchantura@users.noreply.github.com>
2024-05-22 14:28:34 +0200

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
+// vite.config.js
+
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	kit: {
+		// Add any SvelteKit configuration here
+	},
+	build: {
+		rollupOptions: {
+			// Add 'jsonwebtoken' to the list of external modules
+			external: ['jsonwebtoken']
+		}
+	}
 });
